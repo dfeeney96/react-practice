391fa9a092f1406f3304b83e807fe00a8b6549b7
dfeeney96 <daniellefeeney96@gmail.com>
2021-01-18 21:05:22 +0000

    Added open sourced code

---
+++ src/footing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footing(){
+    return(
+       <div>
+
+           <a href="https://github.com/dfeeney96/react-practice" target="_blank"> Access Code </a>
+
+       </div> 
+    )
+}
