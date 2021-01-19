bf20500a457cc95ce9394f5e9e6c7838f3a81286
dfeeney96 <daniellefeeney96@gmail.com>
2021-01-19 08:12:13 +0000

    Added nonref to link

--- src/footing.js
+++ src/footing.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Footing(){
     return(
-       <div>
+       <div className = "link">
 
-           <a href="https://github.com/dfeeney96/react-practice" target="_blank"> Access Code </a>
+           <a href="https://github.com/dfeeney96/react-practice" target="_blank" rel="noreferrer"> Access Code </a>
 
        </div> 
     )
